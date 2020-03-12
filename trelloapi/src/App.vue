a53c7bdbd066a8816aad5e4c8e751ba31cf898fc
nivaaz <nksehmbhi@gmail.com>
2020-03-12 13:10:19 +1100

    added fetch and post

--- trelloapi/src/App.vue
+++ trelloapi/src/App.vue
@@ -1,41 +1,24 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <InputArray 
-    :boards="boards" 
-    @delete:board="deleteBoard"
-    @edit:board="editBoard"
-
-     />
-    <AddForm @add:board="addBoard"/>
+   
+    <TrelloPop />
   </div>
 </template>
 
 <script>
-import InputArray from './components/input_array.vue'
-import AddForm from './components/AddForm.vue'
 
+import TrelloPop from './components/TrelloPop.vue';
 
 export default {
   name: 'App',
   components: {
-    InputArray, 
-    AddForm
+    TrelloPop
   }, 
  data(){
    return {
-      boards:[
-        {
-          id: 0, 
-          name: "boardOne",
-          cardsNumber: "22",
-        },
-        {
-          id: 1, 
-          name: "boardTwo",
-          cardsNumber: "2",
-        },
-           ]
+     token: ' ',
+     boards : []
    }
  },
   mounted() {
@@ -47,34 +30,15 @@ export default {
         // const API_BASE = "https://api.trello.com/1/";
         const API_ME_BASE = "https://api.trello.com/1/members/me/";
         const KEY = "47836fa3a77b0663415e2e5f7152222b";
-        const TOKEN = "XXXX";
+        const TOKEN = this.token;
         const response = await fetch(`${API_ME_BASE}boards?key=${KEY}&token=${TOKEN}&fields=name`);
         const data = await response.json()
         console.log(data)
       } catch (error) {
         console.log("error")
        console.error(error)
-  }
+    }
    },
-   deleteBoard(id) {
-    this.boards = this.boards.filter(
-      board => board.id !== id
-    )
-  },
-  editBoard(id, updatedBoard){
-   this.boards = this.boards.map(board =>
-    board.id === id ? updatedBoard : board
-  )
-  },
-   addBoard(board){
-     const lastId =
-    this.boards.length > 0
-      ? this.boards[this.boards.length - 1].id
-      : 0;
-  const id = lastId + 1;
-    const newBoard = { ...board, id };
-     this.boards =[...this.boards, newBoard]
-   }
  }
 }
 
---
+++ trelloapi/src/components/TrelloPop.vue
@@ -0,0 +1,290 @@
+<template>
+<div>
+
+    <div class ="renderBox">
+    <div>
+        <h1> Render board </h1> 
+        <div v-for="board in boards" :key="board">
+        <p> {{board.name}} {{board.id}} </p>
+        </div>
+    </div>
+    <div>
+        <h1> Render card </h1> 
+        <div v-for="card in cards" :key="card">
+        <p> {{card.name}} {{card.id}} </p>
+        </div>
+    </div>
+    <div>
+        <h1> Render list </h1> 
+        <div v-for="list in lists" :key="list">
+        <p> {{list.name}} {{list.id}} </p>
+        </div>
+    </div>
+    
+    </div>
+
+    <div class="inputBoxes">
+        <form @submit.prevent="handleSubmit">
+            <h2> Add Token </h2> 
+            <input 
+                v-model="token" 
+                type="text"
+                ref="token"
+                />
+            <button > Add </button>
+        </form>
+
+        <h2> Add Board Id </h2> 
+        <input 
+            v-model="boardid" 
+            type="text"
+            ref="board_id"
+            />
+        <button @click = "getCards"> Get Cards </button> 
+    
+        <h2> Add List id </h2> 
+        <input 
+            v-model="list_id" 
+            type="text"
+            ref="list_id"
+            />
+        <button @click = "addListid"> Add </button> 
+
+        <h2> Add name for card</h2> 
+        <input 
+            v-model="card_id" 
+            type="text"
+            ref="card_id"
+            />
+        <button @click = "makeCard"> Add </button> 
+        <label> Each card name should be seperated by a comma</label>
+        <button @click = "addCardsList"> Add List of Cards </button> 
+    </div>
+
+</div>
+</template>
+
+<script>
+export default {
+    name: 'TrelloPop',
+   data(){
+       return {
+            token:'',
+            boardid:'',
+            listid:'',
+            cardid:' ',
+            cards:[],
+            boards: [], 
+            lists: [],
+       }
+   },
+   methods:{
+       handleSubmit(){
+           console.log("handling Submit")
+           console.log(this.token)
+           this.getBoards();
+       },
+       handlecardid(name){
+           console.log(name)
+           const card = {
+               name: name.card_id
+            }
+           this.sendCard(card);
+       },
+       
+       addBoards(data){
+         const dt = data.map((val)=>{
+             const b = {
+                 name: val.name,
+                 id: val.id,
+             }
+             return b
+         })
+         console.log("dt")
+         console.log(dt)
+         this.boards = dt
+       },
+        addCards(data){
+            console.log(this.cardid)
+         const dt = data.map((val)=>{
+             const b = {
+                 name: val.name,
+                 id: val.id,
+             }
+             return b
+         })
+         console.log("dt")
+         console.log(dt)
+         this.cards = dt
+       },
+       addLists(data){
+        const dt = data.map((val)=>{
+            const b = {
+                name: val.name,
+                id: val.id,
+            }
+        return b
+         })
+         console.log("dt")
+         console.log(dt)
+         this.lists = dt
+       },
+       addListid(){
+           console.log(this.list_id)
+           this.listid = this.list_id;
+       },
+        makeCard(){
+          console.log(this.card_id);
+          const card = {name: this.cardid};
+          this.postCard(card);
+       },
+       addCardsList(){
+          console.log(this.card_id);
+        const cards = this.card_id;
+        const cardsList = cards.split(",")
+           cardsList.map((val) =>{
+             this.postCard({name: val});
+           }) 
+       },
+    async getBoards(){
+      try {
+        const API_ME_BASE = "https://api.trello.com/1/members/me/";
+        const KEY = "47836fa3a77b0663415e2e5f7152222b";
+        const TOKEN = this.token;
+        const response = await fetch(`${API_ME_BASE}boards?key=${KEY}&token=${TOKEN}&fields=name`);
+        const data = await response.json()
+        console.log(data)
+        this.addBoards(data)
+
+      } catch (error) {
+        console.log("error")
+       console.error(error)
+    }
+   },
+   async getCards(){
+      try {
+        const API_ME_BASE = "https://api.trello.com/1/boards/";
+        const API_KEY = "47836fa3a77b0663415e2e5f7152222b";
+        const TOKEN = this.token;
+        const BOARDID = this.boardid;
+        const response = await fetch(`${API_ME_BASE}${BOARDID}/cards/?limit=2&fields=name&members=true&member_fields=fullName&key=${API_KEY}&token=${TOKEN}`);
+        const data = await response.json()
+        console.log(data)
+        this.addCards(data)
+        // get the lists 
+        this.getLists();
+
+      } catch (error) {
+        console.log("error")
+       console.error(error)
+    }
+   },
+   async getLists(){
+    try {
+        const API_ME_BASE = "https://api.trello.com/1/boards/";
+        const API_KEY = "47836fa3a77b0663415e2e5f7152222b";
+        const TOKEN = this.token;
+        const BOARD_ID = this.boardid;
+        const response = await fetch(`${API_ME_BASE}${BOARD_ID}/lists?cards=none&card_fields=all&filter=open&fields=all&key=${API_KEY}&token=${TOKEN}`);
+        const data = await response.json()
+        console.log(data)
+        this.addLists(data)
+
+      } catch (error) {
+        console.log("error")
+       console.error(error)
+    }
+   },
+   async postCard(card){
+    console.log(card)
+    const data = null;
+    const API_ME_BASE = "https://api.trello.com/1/cards?name=";
+    const API_KEY = "47836fa3a77b0663415e2e5f7152222b";
+    const TOKEN = this.token;
+    const LIST_ID = this.listid;
+    const CARD_NAME = card.name;
+    const URL_SOURCE = "zaaaaavin.com";
+
+    var xhr = new XMLHttpRequest();
+
+    xhr.addEventListener("readystatechange", function () {
+    if (this.readyState === this.DONE) {
+        console.log(this.responseText);
+    }
+    });
+
+    xhr.open("POST", `${API_ME_BASE}${CARD_NAME}&idList=${LIST_ID}&urlSource=${URL_SOURCE}&keepFromSource=all&key=${API_KEY}&token=${TOKEN}`);
+
+    xhr.send(data);
+   },
+   }
+}
+</script>
+
+<style scoped>
+h1{
+    color: crimson;
+    font-size: medium;
+    text-transform: uppercase;  
+}
+button{
+    color: crimson;
+    text-transform: uppercase;
+    background-color: white;
+    border: 2px solid crimson;
+    border-radius: 1em;
+    padding: 1em;
+    font-weight: bold;
+}
+input{
+    color: crimson;
+    border: 1px solid lightgray;
+    border-radius: 1em;
+    padding: 1em;
+}
+form {
+    text-align: left;
+    display: grid;
+    grid-gap: 1em;
+    color: crimson;
+    font-weight: bold;
+  }
+
+
+[class*='-message'] {
+    font-weight: 500;
+}
+
+.error-message {
+color: #d33c40;
+}
+
+.success-message {
+color: #32a95d;
+}
+input{
+    padding: 1em;
+}
+button{
+    color: crimson;
+    text-transform: uppercase;
+    background-color: white;
+    border: 2px solid crimson;
+    border-radius: 1em;
+    padding: 1em;
+}
+tr{
+    border: 2px solid crimson;
+}
+.renderBox{
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr;
+}
+.inputBoxes{
+    display: grid;
+    grid-template-row: 1fr 1fr 1fr;
+    margin: 1em;
+    color: crimson;
+    text-align: left;
+}
+</style>
